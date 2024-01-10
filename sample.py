9565cc114975c8bee538a7dd9adc883b65e79210
elleklinton <ellek.linton@gmail.com>
2024-01-10 10:27:57 -0800

    Updated sample to point to colab notebook

--- sample.py
+++ sample.py
@@ -1,13 +1,1 @@
-import pied_poker as pp
-
-n_rounds = 50
-n_jobs = -1
-pp.visualization.StartingCardProbabilities(suited=False).visualize(n_rounds=n_rounds, n_jobs=n_jobs)
-pp.visualization.StartingCardProbabilities(suited=True).visualize(n_rounds=n_rounds, n_jobs=n_jobs)
-pp.visualization.PocketPairsVsNumPlayers().visualize(n_rounds=n_rounds,
-                                                         min_players=2,
-                                                         max_players=9,
-                                                         n_jobs=n_jobs)
-#
-#
 ## See https://colab.research.google.com/drive/1sLgDZRGmRojkJUEcHwz9o1j9ZN2j4l0p?usp=sharing
